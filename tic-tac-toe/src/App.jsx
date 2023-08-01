1086eda19a4ae4ac7a65339ef457f2c34136fe6b
rizal15D <muhammadrizal15122001@gmail.com>
2023-08-01 13:00:04 +0700

    firts commit

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+function Square({value, onSquareClick}){
+  return (
+    <button className='square' onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [square,setSquare] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares = square.slice();
+    nextSquares[i] = 'X';
+    setSquare(nextSquares);
+  }
+
+  return (
+    <div className='board'>
+      <Square value={square[0]} onSquareClick={() => handleClick(0)}/>
+      <Square value={square[1]} onSquareClick={() => handleClick(1)}/>
+      <Square value={square[2]} onSquareClick={() => handleClick(2)}/>
+      <Square value={square[3]} onSquareClick={() => handleClick(3)}/>
+      <Square value={square[4]} onSquareClick={() => handleClick(4)}/>
+      <Square value={square[5]} onSquareClick={() => handleClick(5)}/>
+      <Square value={square[6]} onSquareClick={() => handleClick(6)}/>
+      <Square value={square[7]} onSquareClick={() => handleClick(7)}/>
+      <Square value={square[8]} onSquareClick={() => handleClick(8)}/>
+    </div>
+  )
+}
